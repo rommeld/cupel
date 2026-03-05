d877e388bdaa6c2b1a7e83efc281d7af6b8f4451
rommeld <rommel.denny@gmail.com>
2026-03-05 22:23:44 +0100

    create and run application
    - define key bindings
    - add text input
    - stream key strokes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+use cupel::app::{
+    Backspace, Copy, Cut, Delete, End, Home, InputExample, Left, Paste, Quit, Right, SelectAll,
+    SelectLeft, SelectRight, ShowCharacterPalette, TextInput,
+};
+use gpui::{
+    App, AppContext, Application, Bounds, KeyBinding, WindowBounds, WindowOptions, px, size,
+};
+
 fn main() {
-    println!("Hello, world!");
+    Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(300.), px(300.)), cx);
+        cx.bind_keys([
+            KeyBinding::new("backspace", Backspace, None),
+            KeyBinding::new("delete", Delete, None),
+            KeyBinding::new("left", Left, None),
+            KeyBinding::new("right", Right, None),
+            KeyBinding::new("shift-left", SelectLeft, None),
+            KeyBinding::new("shift-right", SelectRight, None),
+            KeyBinding::new("cmd-a", SelectAll, None),
+            KeyBinding::new("cmd-v", Paste, None),
+            KeyBinding::new("cmd-c", Copy, None),
+            KeyBinding::new("cmd-x", Cut, None),
+            KeyBinding::new("home", Home, None),
+            KeyBinding::new("end", End, None),
+            KeyBinding::new("ctrl-cmd-space", ShowCharacterPalette, None),
+        ]);
+
+        let window = cx
+            .open_window(
+                WindowOptions {
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    ..Default::default()
+                },
+                |_, cx| {
+                    let text_input = cx.new(|cx| TextInput {
+                        focus_handle: cx.focus_handle(),
+                        content: "".into(),
+                        placeholder: "Type here...".into(),
+                        selected_range: 0..0,
+                        selection_reversed: false,
+                        marked_range: None,
+                        last_layout: None,
+                        last_bounds: None,
+                        is_selecting: false,
+                    });
+                    cx.new(|cx| InputExample {
+                        text_input,
+                        recent_keystrokes: vec![],
+                        focus_handle: cx.focus_handle(),
+                    })
+                },
+            )
+            .expect("Expected text example.");
+        let view = window.update(cx, |_, _, cx| cx.entity()).expect("");
+
+        cx.observe_keystrokes(move |ev, _, cx| {
+            view.update(cx, |view, cx| {
+                view.recent_keystrokes.push(ev.keystroke.clone());
+                cx.notify();
+            })
+        })
+        .detach();
+
+        cx.on_keyboard_layout_change({
+            move |cx| {
+                window.update(cx, |_, _, cx| cx.notify()).ok();
+            }
+        })
+        .detach();
+
+        window
+            .update(cx, |view, window, cx| {
+                window.focus(&view.text_input.read(cx).focus_handle);
+                cx.activate(true);
+            })
+            .expect("");
+        cx.on_action(|_: &Quit, cx| cx.quit());
+        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+    });
 }
