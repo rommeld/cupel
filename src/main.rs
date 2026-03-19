f754fdc8dbbdf16b65373cc82044eb4da9938e77
rommeld <rommel.denny@gmail.com>
2026-03-19 22:00:34 +0100

    reset project structure

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-use gpui::{AppContext, Application, KeyBinding, WindowOptions};
-
-use cupel::actions::{
-    Commit, Quit, SelectNext, SelectPrev, StageAll, ToggleStaging, UnstageAll,
-};
-use cupel::ui::app::AppView;
-use cupel::assets::Assets;
-use cupel::theme::Theme;
-
 fn main() {
-    Application::new()
-        .with_assets(Assets)
-        .run(|cx| {
-            cx.set_global(Theme::default());
-
-            cx.bind_keys([
-                KeyBinding::new("cmd-q", Quit, None),
-                KeyBinding::new("enter", ToggleStaging, Some("git_panel")),
-                KeyBinding::new("down", SelectNext, Some("git_panel")),
-                KeyBinding::new("up", SelectPrev, Some("git_panel")),
-                KeyBinding::new("cmd-shift-a", StageAll, Some("git_panel")),
-                KeyBinding::new("cmd-shift-u", UnstageAll, Some("git_panel")),
-                KeyBinding::new("cmd-enter", Commit, Some("git_panel")),
-            ]);
-
-            cx.on_action(|_: &Quit, cx| {
-                cx.quit();
-            });
-
-            cx.open_window(WindowOptions::default(), |window, cx| {
-                cx.new(|cx| AppView::new(window, cx))
-            })
-            .expect("failed to open window");
-        });
+    println!("Hello, cupel.")
 }
